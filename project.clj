90cac0b4aaf6cb3334dd6bef9a1db9b29c789d1b
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-11 11:36:30 +0200

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject iloveponies.tests/looping-is-recursion "0.1.0-SNAPSHOT"
+  :description "tests for looping-is-recursion"
+  :url "http://github.com/iloveponies/looping-is-recursion"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [midje "1.5.1"]
+                 [midje-grader "0.1.0-SNAPSHOT"]])
